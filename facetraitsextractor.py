d5e1abbe6765d5036b067bf263d70754fa4a3612
Luis Enrique <luisenriquecgongora@gmail.com>
2018-08-05 14:01:26 -0500

    Face traits extractor already included on faceminer

--- facetraitsextractor.py
+++ facetraitsextractor.py
@@ -1,6 +1,7 @@
 #Make 3D Stack
 ## Append 2D Arrays into 3D Array
 
+#
 #F: Process Section
 ## Autoscale Picture 50x50
 ## Detect Borders
@@ -11,4 +12,3 @@
 ## Detect Mouth
 ## Pre-process Eyes and Mouth
 ## Append Eyes and Mouth into 3D Stack
-
